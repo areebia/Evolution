16321ada80ff402a6872998fe72006ec3ddf6cc4
Areeb Yaqub <aygohan@gmail.com>
2020-07-07 16:04:15 -0700

    Issue fixed
    same code, just fixed an issue regarding local paths

--- tutorial.py
+++ tutorial.py
@@ -4,4 +4,16 @@ window = pyglet.window.Window(800, 600, "Evolution")
 window.set_caption("abcdefghijlmoo")
 window.set_mouse_visible(True)
 
+#pic = pyglet.resource.image('pop.png')
+
+pic = pyglet.image.load("pop.png")
+
+
+@window.event
+def on_draw():
+    window.clear()
+    pic.blit(0,0)
+ 
+
+
 pyglet.app.run()

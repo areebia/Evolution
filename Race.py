469e7e1eca3f1e7c838c550d6e91046b653dd567
Areeb Yaqub <aygohan@gmail.com>
2020-07-16 01:56:28 -0700

    finished race.py

--- Race.py
+++ Race.py
@@ -1,2 +1,14 @@
-Class Race:
-def __init__(self, name, hp, atk, defense)
+#Parent class for all races.
+
+
+class Race:
+    def __init__(self, name, hp, atk, defense, atkSpd, intel, amount, birRate, exp):
+        self.name = name
+        self.hp = hp
+        self.atk = atk
+        self.defense = defense
+        self.atkSpd = atkSpd
+        self.intel = intel
+        self.amount = amount
+        self.birRate = birRate
+        self.exp = exp
